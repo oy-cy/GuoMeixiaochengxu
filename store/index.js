64b76834b0377882cd93425a7966abf8fdcdb9be
cxl <cxl@qq.com>
2020-10-19 10:52:02 +0800

    新增vuex

---
+++ store/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+    //数据
+	state: {
+		
+	},
+	// 存放获取数据的方法
+	getters:{
+
+	},
+	//异步修改
+    actions: {}
+})
+
+export default store
