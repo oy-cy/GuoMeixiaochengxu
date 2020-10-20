4f2ce82d658b9519a5cf810196370685eed81f79
luoyi <2807838041@qq.com>
2020-10-20 19:48:38 +0800

    连接接口文件

--- api/common.js
+++ api/common.js
@@ -2,6 +2,27 @@ import request from "./request.js"
 // 获取轮播图
 export function getLunbotu(type){
 	return request({
-		url:"/getLunbo?/type="+type
+		url:"/getLunbo?type="+type
 	})
 }
+
+// 获取九宫格
+export function getGrid(type){
+	return request({
+		url:"/getGrid?type="+type
+	})
+}
+
+// 秒杀专场
+export function getSeckill(){
+	return request({
+		url:"/getSeckill"
+	})
+}
+
+// 猜你喜欢
+export function getguessLike(){
+	
+}
+
+
--- api/config.js
+++ api/config.js
@@ -1,1 +1,1 @@
-export const url = "http://192.168.9.15:7000/";
+export const url = "http://192.168.9.15:7000";
