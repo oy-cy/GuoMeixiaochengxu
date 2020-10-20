fafc715553c59be90f605ead67fd047bc853fa5b
cxl <cxl@qq.com>
2020-10-20 23:01:46 +0800

    test

---
+++ pages/user/orderList/orderList.vue
@@ -0,0 +1,30 @@
+<template>
+	<view class="order-list-container">
+		<!-- 顶部导航栏 -->
+		<view class="">
+			
+		</view>
+		<!-- <view class="my-order-list" v-if="">
+			<view class="list" v-for="">
+				
+			</view>
+		</view> -->
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
