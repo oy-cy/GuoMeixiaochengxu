ae9923810c0f1313f2d8aa01e789e4fca768183f
cxl <cxl@qq.com>
2020-10-23 21:06:28 +0800

    生成订单时操作地址管理，返回生成订单页面

--- pages/site/editAddr/editAddr.vue
+++ pages/site/editAddr/editAddr.vue
@@ -1,6 +1,6 @@
 <template>
 	<view class="add-addr-container">
-		<save-addr isEdit="true" :addrId="id"/>
+		<save-addr isEdit="true" :addrId="id" :car="car"/>
 	</view>
 </template>
 
@@ -9,11 +9,15 @@ import saveAddr from '@/component/site/saveAddr.vue';
 	export default {
 		data() {
 			return {
-				id:''
+				id:'',
+				car:false
 			}
 		},
 		onLoad(option) {
 			this.id = option.id;
+			if(option.car == true || option.car == 'true'){
+				this.car = option.car;
+			}
 		},
 		components:{
 			saveAddr
